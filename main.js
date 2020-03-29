59b38066cef6d91737fc1cd5dbd46ba457474f86
Nahidul <nahisl12@gmail.com>
2020-03-29 18:06:38 +0100

    Added allowing the user to choose size of the grid, added function to dynamically resize.

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const mainDiv = document.querySelector('.main');
 
-function createGrid() {
-     for(let i = 0; i < 16; i++) {
-         for(let j = 0; j < 16; j++){
+function createGrid(row, col) {
+     for(let i = 0; i < row; i++) {
+        mainDiv.style.setProperty('--grid-rows', row);
+        mainDiv.style.setProperty('--grid-cols', col);
+         for(let j = 0; j < col; j++){
             const row = document.createElement('div');
             row.className = "row";
             mainDiv.appendChild(row);
         }
     }
 }
+
+// function createBoard(row, col) {
+//     mainDiv.style.setProperty('--grid-rows', row);
+//     mainDiv.style.setProperty('--grid-cols', col);
+//     for(let i = 0; i < (row*col); i++) {
+//         const box = document.createElement('div');
+//         box.innerText = (i + 1);
+//         box.className = "cell";
+//         mainDiv.appendChild(box);
+//     }
+// }
+
+// createBoard(16,16);
+
 
 // used to change the colorof the divs
 mainDiv.addEventListener('mouseover', function(event){
@@ -19,34 +35,22 @@ mainDiv.addEventListener('mouseover', function(event){
     }
  });
 
- // RESET BUTTON
+ // RESET BUTTON AND REDRAW BASED ON INPUTTED SIZE
  const resetBtn = document.querySelector('.resetGrid');
  resetBtn.addEventListener('click', function(){
     let child = mainDiv.lastElementChild;
     while(child){
         mainDiv.removeChild(child);
         child = mainDiv.lastElementChild;
-        console.log("w");
     }
-    createGrid();
+
+    let userChoice = prompt("how big would you like the grid to be?");
+    if(userChoice <= 64){
+    createGrid(userChoice,userChoice);
+    } else {
+        let userChoice = prompt("how big would you like the grid to be? DO NOT EXCEED 64");
+    }
  });
 
 
- createGrid();
-
-// function hoverColor() {
-//     row.style.backgroundColor = 'red';
-//     console.log("working");
-// }
-
-// function createBoard() {
-//     for(let i = 0; i < (16*16); i++) {
-//         const box = document.createElement('div');
-//         box.innerText = (i + 1);
-//         box.className = "cell";
-//         mainDiv.appendChild(box);
-//     }
-// }
-
-// createBoard();
-
+ createGrid(16,16);
